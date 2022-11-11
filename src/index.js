fb148baa0c8555ba54411fdf5a4755e7b3c0966c
Student <student@epicodus-5.hsd1.or.comcast.net>
2022-11-10 16:19:33 -0800

    Finalize game and damage output
    Co-authored-by: Lucas Mollerstuen <mollerstuen.works@gmail.com>

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ import OctoSlash from './js/badies/cave/octoSlash.js';
 import NyteRat from './js/badies/cave/nyterat.js';
 import Gargoyle from './js/badies/church/gargoyle.js';
 import UnholyPriest from './js/badies/church/unholypriest.js';
+import octoSlash from './assets/images/octoslash.png';
+import nyteRat from './assets/images/nyterat.png';
+import gargoyle from './assets/images/gargoyle.png';
+import unholy from './assets/images/unholy.png';
 
 let currCharacter;
 let currBadie;
@@ -80,10 +84,10 @@ const handleEngage = (event) => {
   if (currCharacter.location === "cave") {
     if (randomizer <= 50) {
       currBadie = new OctoSlash();
-      document.getElementById("badieImg").setAttribute("src", "assets/images/octoslash.png");
+      document.getElementById("badieImg").setAttribute("src", octoSlash);
     } else {
       currBadie = new NyteRat();
-      document.getElementById("badieImg").setAttribute("src", "assets/images/nyterat.png");
+      document.getElementById("badieImg").setAttribute("src", nyteRat);
     }
     document.getElementById('badieName').setAttribute('style', 'color: black;')
     document.getElementById("badieName").innerText = currBadie.name;
@@ -91,10 +95,10 @@ const handleEngage = (event) => {
   } else {
     if (randomizer <= 70) {
       currBadie = new Gargoyle();
-      document.getElementById("badieImg").setAttribute("src", "assets/images/gargoyle.png");
+      document.getElementById("badieImg").setAttribute("src", gargoyle);
     } else {
       currBadie = new UnholyPriest();
-      document.getElementById("badieImg").setAttribute("src", "assets/images/unholy.png");
+      document.getElementById("badieImg").setAttribute("src", unholy);
     }
     document.getElementById('badieName').setAttribute('style', 'color: black;')
     document.getElementById("badieName").innerText = currBadie.name;
@@ -115,12 +119,16 @@ const handleLeave = (event) => {
 
 const handleAttack = (event) => {
   event.preventDefault();
+  let curAttack = currCharacter.attack(currCharacter.chance());
   document.getElementById("dead").innerText = null;
-  currBadie.health -= currCharacter.attack(currCharacter.chance());
+  currBadie.health -= curAttack;
+  document.getElementById("badieAtkDamage").innerHTML = `-${curAttack}`;
   document.getElementById("badieHealth").innerText = `Health: ${currBadie.health}`;
   if (!currBadie.isDead()) {
-    currCharacter.health -= currBadie.attack(currBadie.chance());
+    curAttack = currBadie.attack(currBadie.chance())
+    currCharacter.health -= curAttack;
     document.getElementById("heroHealth").innerText = currCharacter.health;
+    document.getElementById("atkDamage").innerHTML = `-${curAttack}`;
   } else {
     currCharacter.exp += currBadie.exp;
     currCharacter.lvlUp();
@@ -140,6 +148,8 @@ const handleAttack = (event) => {
     document.getElementById('heroMana').innerHTML = currCharacter.mana;
     document.getElementById('badieName').setAttribute('style', 'color: red;')
     document.getElementById("badieName").innerText = "DEAD MEAT!";
+    document.getElementById("badieAtkDamage").innerHTML = null;
+    document.getElementById("atkDamage").innerHTML = null;
     document.getElementById('badieHealth').innerHTML = null;
     document.getElementById('choice').removeAttribute('class', 'hidden');
     document.getElementById('moves').setAttribute('class', 'hidden');
@@ -154,17 +164,22 @@ const handleAttack = (event) => {
 
 const handleMagic = (event) => {
   event.preventDefault();
+  let curAttack;
   if (currCharacter.mana < 3) {
     document.getElementById("dead").innerText = 'NOT ENOUGH MANA!';
   } else {
     document.getElementById("dead").innerText = null;
     currCharacter.mana -= 3;
     document.getElementById('heroMana').innerHTML = currCharacter.mana;
-    currBadie.health -= currCharacter.magic(currCharacter.chance())
+    curAttack = currCharacter.magic(currCharacter.chance());
+    currBadie.health -= curAttack;
     document.getElementById("badieHealth").innerText = `Health: ${currBadie.health}`;
+    document.getElementById("badieAtkDamage").innerHTML = `-${curAttack}`;
     if (!currBadie.isDead()) {
-      currCharacter.health -= currBadie.attack(currBadie.chance())
+      curAttack = currBadie.attack(currBadie.chance());
+      currCharacter.health -= curAttack;
       document.getElementById("heroHealth").innerText = currCharacter.health;
+      document.getElementById("atkDamage").innerHTML = `-${curAttack}`;
     } else {
       currCharacter.exp += currBadie.exp;
       currCharacter.lvlUp();
@@ -176,8 +191,10 @@ const handleMagic = (event) => {
       document.getElementById('heroMana').innerHTML = currCharacter.mana;
       document.getElementById('heroExp').innerHTML = currCharacter.exp;
       document.getElementById("heroExp").innerText = currCharacter.exp;
-      document.getElementById('badieName').setAttribute('style', 'color: red;')
+      document.getElementById('badieName').setAttribute('style', 'color: red;');
       document.getElementById("badieName").innerText = "DEAD MEAT!";
+      document.getElementById("atkDamage").innerHTML = null;
+      document.getElementById("badieAtkDamage").innerHTML = null;
       if (currCharacter.mana + 3 < currCharacter.maxMana) {
         currCharacter.mana += 3;
       } else {
@@ -190,6 +207,7 @@ const handleMagic = (event) => {
     }
     if (currCharacter.isDead()) {
       document.getElementById("dead").innerText = 'DEAD MEAT!';
+      document.getElementById('heroHealth').innerHTML = 0;
       document.getElementById('moves').setAttribute('class', 'hidden');
       document.getElementById('reset').removeAttribute('class', 'hidden');
     }
