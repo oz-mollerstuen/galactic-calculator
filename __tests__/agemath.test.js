aa57351b805712a69aadc62ce053016850fe0bde
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-11-14 09:43:05 -0800

    red test for mercury

--- __tests__/agemath.test.js
+++ __tests__/agemath.test.js
@@ -6,4 +6,10 @@ describe('UserAge', () => {
     let input = new UserAge(36);
     expect(input.age).toEqual(36);
   });
+
+  test('It should use age imput to find age on Mercury', () => {
+    let input = new UserAge(36);
+    expect(input.findMercAge()).toEqual(8.6)
+  })
 })
+
