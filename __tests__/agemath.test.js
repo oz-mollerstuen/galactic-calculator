9be722875376f8b524f8a72a486b31b80971ff70
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-11-14 10:29:04 -0800

    red test for years till average death on earth

--- __tests__/agemath.test.js
+++ __tests__/agemath.test.js
@@ -26,6 +26,12 @@ describe('UserAge', () => {
     let input = new UserAge(36);
     expect(input.findJupAge()).toEqual(427)
   });
+
+  test('On Earth, it should tell user how many years until they are 85, as well as how many till 85', () => {
+    let input = new UserAge(36);
+    expect(input.earthYears()).toEqual(49)
+  })
+
 })
 
 
