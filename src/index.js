8cb70ac31ca356507710434fb8803f7b4af3ceca
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-11-14 14:05:05 -0800

    add index.js coding to start outputing ages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import UserAge from './js/UserAge';
 
+function handleform (event) {
+  event.preventDefault();
+  let currAge = document.getElementById('input1').value;
+  let uAge = new UserAge(currAge)
+  let mercCurr = 
+  document.getElementById('p1').innerHTML = uAge.
+
+
+}
+
+
+
+
+
+window.addEventListener('load', function() {
+  this.document.querySelector("form").addEventListener("submit", handleform)
+});
